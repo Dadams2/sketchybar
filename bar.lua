0321b181104f1612ec86a03b1095740a312ee58d
David Adams <david@dadams2.com>
2025-07-04 15:09:01 +1000

    custom padding

--- bar.lua
+++ bar.lua
@@ -2,8 +2,10 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-  height = 40,
+  height = 35,
   color = colors.bar.bg,
-  padding_right = 2,
-  padding_left = 2,
+  padding_right = 8,
+  padding_left = 8,
+  y_offset = -3,
+  margin=-2,
 })
